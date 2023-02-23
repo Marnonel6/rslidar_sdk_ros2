5fa67fb6ce55577219b19465fea1ffdaf5470438
Nick Morales <ngmorales97@gmail.com>
2023-02-23 12:32:34 -0600

    Changed fixed frame for RVIZ and made launching it optional

--- launch/start.py
+++ launch/start.py
@@ -1,12 +1,36 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-
-    rviz_config=get_package_share_directory('rslidar_sdk')+'/rviz/rviz2.rviz'
-
     return LaunchDescription([
-        Node(namespace='rslidar_sdk', package='rslidar_sdk', executable='rslidar_sdk_node', output='screen'),
-        Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
+        DeclareLaunchArgument(
+            name='use_rviz',
+            default_value='true',
+            choices=['true','false'],
+            description='Open RVIZ for visualization'
+        ),
+        Node(
+            namespace='rslidar_sdk',
+            package='rslidar_sdk',
+            executable='rslidar_sdk_node',
+            output='screen'
+        ),
+        Node(
+            namespace='rviz2',
+            package='rviz2',
+            executable='rviz2',
+            arguments=[
+                '-d',
+                PathJoinSubstitution([
+                    FindPackageShare('rslidar_sdk'),
+                    'rviz',
+                    'rviz2.rviz'
+                ])
+            ],
+            condition=IfCondition(LaunchConfiguration('use_rviz'))
+        ),
     ])
